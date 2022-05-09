6622762f2935e128cf5597764e7845f6c1b119b0
NGUYEN KHA HOP <1721050681@student.humg.edu.vn>
2022-05-09 14:57:35 +0700

    update

--- DoAnTotNghiep01/WareHouse/src/environments/environment.ts
+++ DoAnTotNghiep01/WareHouse/src/environments/environment.ts
@@ -4,9 +4,12 @@
 
 export const environment = {
   production: false,
-  baseApi:"https://localhost:5005/api/v1/",
-  authorizeApi:"https://localhost:5001/api/v1/",
-  baseSignalr:"https://localhost:5001/",
+  // baseApi:"http://localhost:5005/api/v1/",
+  // authorizeApi:"http://localhost:5001/api/v1/",
+  // baseSignalr:"http://localhost:5001/",
+  baseApi:"http://localhost:8000/api-wh/api/v1/",
+  authorizeApi:"http://localhost:8000/api-mt/api/v1/",
+  baseSignalr:"hhttp://localhost:8000/api-wh/",
 };
 
 /*
